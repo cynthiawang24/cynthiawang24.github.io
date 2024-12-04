941d59f6052eb7d3132589d4148ab7ce68c063d7
cynthiawangjy <cynthiawangjy@gmail.com>
2024-12-03 20:54:47 -0500

    all exercises up there!

--- exercises/dark-mode/script.js
+++ exercises/dark-mode/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+	$('.item').click(function() {
+		$(this).toggleClass('active');
+	});
+
   $('button').click(function() {
     $('div').toggle();
     $('button').toggleClass('active'); 
   }); 
+
+	$("#darkmode").click(function() {
+		$('body').toggleClass('darkmode');
+	});
 });
